081390b489f6897754713b525d03da89d886df02
Gloria CG <69868502+GloriaCamposGarcia@users.noreply.github.com>
2026-02-15 13:27:29 -0600

    feat: add execution time measurement

--- computeSales.py
+++ computeSales.py
@@ -1,6 +1,9 @@
 import sys
+import time
 
 def main():
+    start_time = time.time()  # Inicio de tiempo
+
     """Función principal para coordinar la ejecución."""
     # Validar que se reciban los dos archivos como parámetros: python computeSales.py file1 file2
     if len(sys.argv) != 3:
@@ -12,6 +15,9 @@ def main():
     sales_file = sys.argv[2]
 
     print(f"Archivos recibidos: {price_file} y {sales_file}")
+
+    elapsed_time = time.time() - start_time  # Tiempo transcurrido
+    print(f"Time elapsed: {elapsed_time:.4f} seconds")
 
 if __name__ == "__main__":
     main()
